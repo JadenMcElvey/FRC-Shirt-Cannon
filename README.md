ae7850630e3b8b9bebbb6c000c76aa8704642291
Jaden McElvey <32574860+JadenMcElvey@users.noreply.github.com>
2018-08-29 15:16:08 -0400

    Initial Code
    If any modifications are needed they should be pretty minor. This code probably doesn't have any major unforseen issues.

---
+++ README.md
@@ -0,0 +1,2 @@
+# FRC-Power-Up-Team-4112-Offseason
+The code for team 4112's 2018 offseason robot.
---
+++ Robot/cpp/src/cpp/Robot.cpp
@@ -0,0 +1,80 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+
+#include <Drive\MecanumDrive.h>
+#include <Joystick.h>
+#include <XboxController.h>
+#include <SampleRobot.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include <Talon.h>
+#include <Timer.h>
+#include <Relay.h>
+#include <Compressor.h>
+#include <DigitalInput.h>
+
+class Robot : public frc::SampleRobot {
+public:
+	Robot() {
+		robotDrive.SetExpiration(0.1);		
+	}
+
+	void RobotInit() {
+		compressor.Start();
+	}
+
+	void Autonomous() {
+	}
+
+	void OperatorControl() override {
+		robotDrive.SetSafetyEnabled(true);
+		while (IsOperatorControl() && IsEnabled())
+			{
+			// Drive
+			robotDrive.DriveCartesian(controller.GetY(frc::GenericHID::kLeftHand), controller.GetX(frc::GenericHID::kRightHand), controller.GetX(frc::GenericHID::kLeftHand));
+			// Fire
+            bool n = frc::SmartDashboard::GetBoolean("Safety", false);
+			if ((controller.GetTriggerAxis(frc::GenericHID::kLeftHand)>.5) && n)
+			{
+				relay.Set(frc::Relay::Value::kOn);
+			}
+            else
+            {
+                relay.Set(frc::Relay::Value::kOff);
+            }
+			// The motors will be updated every 5ms
+			frc::Wait(0.005);
+		}
+	}
+
+	/*
+	 * Runs during test mode
+	 */
+	void Test() override {}
+
+private:
+	// ROBOT DRIVE SYSTEM
+	frc::Talon f_leftMotor{0};
+	frc::Talon b_leftMotor{1};
+	frc::Talon f_rightMotor{2};
+	frc::Talon b_rightMotor{3};
+
+	frc::MecanumDrive robotDrive{f_leftMotor, b_leftMotor, f_rightMotor, b_rightMotor};
+
+	frc::XboxController controller{0};
+	
+	// Compressor
+	frc::Compressor compressor;
+
+    // Relay
+    frc::Relay relay{0, frc::Relay::kForwardOnly};
+};
+
+START_ROBOT_CLASS(Robot)
